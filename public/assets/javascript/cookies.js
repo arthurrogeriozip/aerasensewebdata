a828b877c7431961fdd4ea549e419ef8d09ed0aa
MarleyS439 <marleysantos439@gmail.com>
2025-11-28 20:47:28 -0300

    Adição de arquivo separado para sessão e banner de cookies. Também link no rodapé para acesso a Central de Suporte

---
+++ public/assets/javascript/cookies.js
@@ -0,0 +1,18 @@
+// Adiciona um evento de ao carregar o documento HTML
+document.addEventListener("DOMContentLoaded", () => {
+  // Banner
+  var banner = document.getElementById("banner");
+
+  // Função para exibir o banner de cookies
+  function exibirBannerCookies() {
+    banner.style.display = "flex";
+  }
+
+  // Função para esconder o banner de cookies
+  function esconderBannerCookies() {
+    banner.style.display = "none";
+  }
+
+  // Chama a função de exibir o banner de cookies, após 2s que a página carrega
+  setTimeout(exibirBannerCookies, 2500);
+});
